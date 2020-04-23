5f8fe536be4b629c1c14745cc22065bd46773192
Dylan Thacker-Smith <Dylan.Smith@shopify.com>
2020-04-23 14:45:52 -0400

    Move AddInitialProducts migration to db/migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_23_161344) do
+ActiveRecord::Schema.define(version: 2020_04_23_161529) do
 
   create_table "product_handles", force: :cascade do |t|
     t.string "handle"
